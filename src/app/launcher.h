5ac9e7aea74687b6fa43cc7fae76bee0b6922247
Alberto-o <albergascon@gmail.com>
2025-11-11 12:48:18 +0100

    Minor

--- src/app/launcher.h
+++ src/app/launcher.h
@@ -6,7 +6,7 @@
 namespace meshlib::app {
 
 const std::string conformal_mesher ("conformal");
-const std::string structured_mesher ("structured");
+const std::string staircase_mesher ("staircase");
 
 int launcher(int argc, const char* argv[]);
 
